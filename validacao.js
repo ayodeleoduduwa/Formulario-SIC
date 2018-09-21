b6cbd1a8b197a1840ee8c1201f4088c7a69c903a
ayodeleoduduwa <ayodeleodududwa52@gmail.com>
2018-09-20 22:07:21 -0300

    Formulario

---
+++ validacao.js
@@ -0,0 +1,118 @@
+function validarformulario() {
+    var n = document.forms["forma1"]["nome"].value;
+    if (n == "") {
+        alert("Nome deve ser preenchido");
+        
+    }
+}
+
+function validarformularioC() {
+    var x = document.forms["forma1"]["cpf"].value;
+    if (x == "") {
+        alert("CPF deve ser preenchido");
+        
+    }
+}
+
+
+function validarformularioD() {
+    var d = document.forms["forma1"]["documento"].value;
+    if (d == "") {
+        alert("Documento deve ser preenchido");
+        
+    }
+}
+
+
+
+function validarformularionu() {
+    var nu = document.forms["forma1"]["nu"].value;
+    if (nu == "") {
+        alert("Numero deve ser preenchido");
+        
+    }
+}
+
+
+
+
+function validarformularioemail() {
+    var ema = document.forms["forma1"]["email"].value;
+    if (ema == "") {
+        alert("E-mail deve ser preenchido ou Estado fisico");
+        
+    }
+	
+	
+}
+
+
+function validarformularioci() {
+    var ci = document.forms["forma1"]["Cidade"].value;
+    if (ci == "") {
+        alert("Cidade deve ser preenchido");
+        
+    }
+}
+
+
+function validarformularioes() {
+    var e = document.forms["forma1"]["Estado"].value;
+    if (e == "") {
+        alert("Estado deve ser preenchido");
+        
+    }
+}
+
+
+function validarformulariouf() {
+    var u = document.forms["forma1"]["UF"].value;
+    if (u == "") {
+        alert("UF deve ser preenchido");
+        
+    }
+}
+
+
+
+
+function validarformularioce() {
+    var ce = document.forms["forma1"]["CEP"].value;
+    if (ce == "") {
+        alert("CEP deve ser preenchido");
+        
+    }
+}
+
+
+function validarformulariona() {
+    var na = document.forms["forma1"]["Nascimento"].value;
+    if (na == "") {
+        alert("Nascimento deve ser preenchido");
+        
+    }
+}
+
+
+function validarformulariop() {
+    var p = document.forms["forma1"]["pais"].value;
+    if (p == "") {
+        alert("Pais deve ser preenchido");
+        
+    }
+}
+
+
+function validarformulariot() {
+    var t = document.forms["forma1"]["tele"].value;
+    if (t == "") {
+        alert("Telefone deve ser preenchido");
+        
+    }
+}
+
+
+
+
+
+
